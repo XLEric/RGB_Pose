2daf0b53d17af4c14c349cec10a0e87f27cab4b6
xiang.li <xiang.li09@bianlifeng.com>
2020-03-06 23:34:38 +0800

    2020-03-06

--- Hand_Detect/ctdet_trainer.py
+++ Hand_Detect/ctdet_trainer.py
@@ -16,7 +16,6 @@ from utils.post_process import ctdet_post_process
 from utils.oracle_utils import gen_oracle_map
 from utils.utils import AverageMeter
 
-
 class ModleWithLoss(torch.nn.Module):
   def __init__(self, model, loss):
     super(ModleWithLoss, self).__init__()
@@ -96,9 +95,6 @@ class BaseTrainer(object):
           print('{}| {}'.format(opt.task, Bar.suffix))
       else:
         bar.next()
-
-      # if opt.debug > 0:
-      #   self.debug(batch, output, iter_id)
 
       if opt.test:
         self.save_result(output, batch, results)
@@ -183,43 +179,6 @@ class CtdetTrainer(BaseTrainer):
     loss_states = ['loss', 'hm_loss', 'wh_loss', 'off_loss']
     loss = CtdetLoss(opt)
     return loss_states, loss
-
-  # def debug(self, batch, output, iter_id):
-  #   opt = self.opt
-  #   reg = output['reg'] if opt.reg_offset else None
-  #   dets = ctdet_decode(
-  #     output['hm'], output['wh'], reg=reg,
-  #     cat_spec_wh=opt.cat_spec_wh, K=opt.K)
-  #   dets = dets.detach().cpu().numpy().reshape(1, -1, dets.shape[2])
-  #   dets[:, :, :4] *= opt.down_ratio
-  #   dets_gt = batch['meta']['gt_det'].numpy().reshape(1, -1, dets.shape[2])
-  #   dets_gt[:, :, :4] *= opt.down_ratio
-  #   for i in range(1):
-  #     debugger = Debugger(
-  #       dataset=opt.dataset, ipynb=(opt.debug==3), theme=opt.debugger_theme)
-  #     img = batch['input'][i].detach().cpu().numpy().transpose(1, 2, 0)
-  #     img = np.clip(((
-  #       img * opt.std + opt.mean) * 255.), 0, 255).astype(np.uint8)
-  #     pred = debugger.gen_colormap(output['hm'][i].detach().cpu().numpy())
-  #     gt = debugger.gen_colormap(batch['hm'][i].detach().cpu().numpy())
-  #     debugger.add_blend_img(img, pred, 'pred_hm')
-  #     debugger.add_blend_img(img, gt, 'gt_hm')
-  #     debugger.add_img(img, img_id='out_pred')
-  #     for k in range(len(dets[i])):
-  #       if dets[i, k, 4] > opt.center_thresh:
-  #         debugger.add_coco_bbox(dets[i, k, :4], dets[i, k, -1],
-  #                                dets[i, k, 4], img_id='out_pred')
-  #
-  #     debugger.add_img(img, img_id='out_gt')
-  #     for k in range(len(dets_gt[i])):
-  #       if dets_gt[i, k, 4] > opt.center_thresh:
-  #         debugger.add_coco_bbox(dets_gt[i, k, :4], dets_gt[i, k, -1],
-  #                                dets_gt[i, k, 4], img_id='out_gt')
-  #
-  #     if opt.debug == 4:
-  #       debugger.save_all_imgs(opt.debug_dir, prefix='{}'.format(iter_id))
-  #     else:
-  #       debugger.show_all_imgs(pause=True)
 
   def save_result(self, output, batch, results):
     reg = output['reg'] if self.opt.reg_offset else None
